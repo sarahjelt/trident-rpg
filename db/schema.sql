7d60c743528c0a0242c1fd8b758a4922f1352f04
Lindsay Wall <lindsay.is@gmail.com>
2017-12-09 12:47:06 -0500

    misc changes to server.js

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE weird_armies_db;
+USE weird_armies_db;
+
+CREATE TABLE users (
+	id int NOT NULL AUTO_INCREMENT,
+	username varchar(50) NOT NULL,
+	game_id int,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE all_games (
+	id int NOT NULL AUTO_INCREMENT,
+	first_player varchar(50) NOT NULL,
+	second_player varchar(50),
+	game_status varchar(10) NOT NULL,
+	first_player_score int NOT NULL,
+	second_player_score int NOT NULL,
+	turn varchar(50) NOT NULL,
+	need_player boolean NOT NULL
+	PRIMARY KEY (id)
+)
+
+-- where game_id in users matches id in games
+-- where first_player matches username
+-- where second_player matches username
