159c58c3f436e7feb898b504a641daefe00fc44e
Sara Hjelt <sarahjelt@guest-2-00303.dhcp.unc.edu>
2017-12-09 13:31:12 -0500

    requires passport in server js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express')
+var session = require('express-session')
 var exphbs = require('express-handlebars')
 var bodyParser = require('body-parser')
-var PORT = 3000
+var PORT = process.env.PORT || 3000
+var passport = require("passport")
+
 
 var app = express()
 
@@ -9,6 +12,10 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({secret: 'hello world', resave: true, saveUninitialized:true}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 var routes = require('./controllers/game-controller.js')
 
