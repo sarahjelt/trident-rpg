8b15bdb1b3ec4a9cfa3a2182ea1ab6483fd4edd1
Lindsay Wall <lindsay.is@gmail.com>
2017-12-09 11:52:40 -0500

    bare bones server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var exphbs = require('express-handlebars')
+var bodyParser = require('body-parser')
+var PORT = 3000
+
+var app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// var routes = require('./controllers/game-controller.js')
+
+// app.use('/', routes)
+
+app.listen(PORT, () => {
+	console.log(`App listening on ${PORT}`)
+})
