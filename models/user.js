62dfb611a746886ad67cb9bf4de306c6f0807655
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-12-10 23:53:06 -0500

    puts some validations back in User model

--- models/user.js
+++ models/user.js
@@ -2,39 +2,39 @@ module.exports = function(sequelize, DataTypes) {
 	var Users = sequelize.define('Users', {
 		email: {
 			type: DataTypes.STRING,
-			// allowNull: false,
-			// validate: {
-			// 	len: [1,50],
-			// 	isemail: true
-			// }
+			allowNull: false,
+			validate: {
+				len: [1,50],
+				isEmail: true
+			}
 		},
 		firstname: {
 			type: DataTypes.STRING,
-			// allowNull: false,
-			// validate: {
-			// 	len: [1,50],
-			// 	isAlphanumeric: true
-			// }
+			allowNull: false,
+			validate: {
+				len: [1,50],
+				isAlphanumeric: true
+			}
 		},
 		lastname: {
 			type: DataTypes.STRING,
-			// allowNull: false,
-			// validate: {
-			// 	len: [1,50],
-			// 	isAlphanumeric: true
-			// }
+			allowNull: false,
+			validate: {
+				len: [1,50],
+				isAlphanumeric: true
+			}
 		},
 		username: {
 			type: DataTypes.STRING,
-			// allowNull: false,
-			// validate: {
-			// 	len: [1,50],
-			// 	isAlphanumeric: true
-			// }
+			allowNull: false,
+			validate: {
+				len: [1,50],
+				isAlphanumeric: true
+			}
 		},
 		password: {
 			type: DataTypes.STRING,
-			// allowNull: false,
+			allowNull: false,
 			// validate: {
 			// 	len: [1,50],
 			// 	isAlphanumeric: true
