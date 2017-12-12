b13f3d25f5f0b87a261daea2da7fc6b0bd84c108
Daniel Blat <blatde91@gmail.com>
2017-12-12 18:34:57 -0500

    had to change file structure

--- public/assets/js/phaserlogic/prefabs/board.js
+++ public/assets/js/phaserlogic/prefabs/board.js
@@ -8,6 +8,16 @@ TheGame.Board = function(state, grid) {
 	this.grid = grid;
 	this.rows = grid.length;
 	this.cols = grid[0].length;
+
+	//make grid
+	var row, col, tile, x, y;
+	for(row = 0; row < this.rows; row++) {
+		for(col = 0; col < this.cols; cols++) {
+			if(row % 2 === 0) {
+				x = this.state.Margin_X + col * this.state.TILE_W;
+			}
+		}
+	}
 }
 
 TheGame.Board.prototype = Object.create(Phaser.Group.prototype);
