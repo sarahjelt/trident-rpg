fda519b45a55d40a731dbe69ee682176713be263
Daniel Blat <blatde91@gmail.com>
2017-12-12 18:47:59 -0500

    added new folder to carry data

--- public/assets/js/phaserlogic/states/game.js
+++ public/assets/js/phaserlogic/states/game.js
@@ -4,10 +4,11 @@ TheGame.GameState = {
 	init: function() {
 		this.TILE_W = 60;
 		this.TILE_H = 68;
-		this.MARGIN_X = 10;
-		this.MARGIN_Y = 2;
+		this.MARGIN_X = 20;
+		this.MARGIN_Y = 5;
 	},
 	create: function() {
+		this.map = JSON.parse(this.game.cache.getText('map'));
 		this.board = new TheGame.Board(this, this.map.grid);
 	}
 
