7b001808701bf2e0fb4d0c2aed44856df447c946
Lindsay Wall <lindsay.is@gmail.com>
2017-12-12 21:11:22 -0500

    removed redundant route from html routing

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -16,39 +16,6 @@ router.get('/game', (request, result) => {
 router.get('/buttons', (request, result) => {
 	result.render('buttons')
 })
-
-// show all active games
-router.get('/games', (request, result) => {
-	console.log('I got to the /api/games get route')
-	db.Games.findAll({
-		where: {
-			need_player: true
-		}
-	}).then(function(dbGames) {
-		console.log('/api/games get all route 0', dbGames[0].dataValues.id)
-		console.log('the length of dbGames is ', dbGames.length)
-		let dbGamesArr = []
-
-		for (var i = 0; i < dbGames.length; i++) {
-			let newObj = {
-				id: dbGames[i].dataValues.id,
-				first_player: dbGames[i].dataValues.first_player
-			}
-
-			dbGamesArr.push(newObj)
-		}
-
-		console.log('dbGamesArr', dbGamesArr)
-
-		let openGamesObj = {
-			games: dbGamesArr
-		}
-
-		console.log('openGamesObj', openGamesObj)
-
-		result.render('openGames', {games: dbGames})
-	})
-})
   
 //html route for about page
 router.get('/about', (req, res) => {
