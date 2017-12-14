0406d2c3f77d2f1b37bc0204cb82578c418289ca
Lindsay Wall <lindsay.is@gmail.com>
2017-12-13 20:00:09 -0500

    commiting for merge

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,3 +1,5 @@
+var db = require('../models') 
+
 var exports = module.exports = {}
  
 exports.signup = function(req, res) {
@@ -13,7 +15,13 @@ exports.signin = function(req, res) {
 }
 
 exports.dashboard = function(req, res) {
-  res.render('dashboard', {username: req.user.username});
+	db.Games.findAll({
+		where: {
+			need_player: true
+		}
+	}).then(function(dbGames) {
+		res.render('dashboard', {username: req.user.username, games: dbGames});
+	})
 }
 
 exports.logout = function(req, res) {
